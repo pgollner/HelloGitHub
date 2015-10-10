1a2e637e7937b6d145514f0dd43c3ae02ca53e6d
Patrick Gollner <patrick.gollner@gmx.at>
2015-10-10 14:52:03 +0200

    anpassungen

--- src/eclipse/HelloGitHub.java
+++ src/eclipse/HelloGitHub.java
@@ -5,14 +5,17 @@ public class HelloGitHub
 
 	public static void main(String[] args) 
 	{
-		//variablen anlegen
-		boolean treeFront = true; // booleanvariable angelegt mit Ausgangswert true
+		// variablen anlegen
+		boolean treeFront = true; // booleanvariable angelegt mit Ausgangswert
+									// true
 		int numberleafes = 0; // intvariable angelegt mit Ausgangswert 0
-		
+
 		// Ausgabe als Text; ohne "ln" gäbe es keinen Zeilenumbruch
-		
+
 		System.out.println("Ist baum voraus? " + treeFront);
 		System.out.println("Anzahl Blätter: " + numberleafes);
-		System.out.println("Anzahl Blätter: " + numberleafes + "; Baum voraus " + treeFront );
-	}	
+		System.out.println();																	//leerzeile
+		System.out.println("Anzahl Blätter: " + numberleafes + "\n" + "Baum voraus " + treeFront);
+		
+	}
 }
