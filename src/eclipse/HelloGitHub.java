2c643256e1e5a7811c08e1f0afcab6faa9cf0792
Patrick Gollner <patrick.gollner@gmx.at>
2015-10-10 14:45:10 +0200

    system.out.printin und variablenanlage

--- src/eclipse/HelloGitHub.java
+++ src/eclipse/HelloGitHub.java
@@ -5,8 +5,14 @@ public class HelloGitHub
 
 	public static void main(String[] args) 
 	{
+		//variablen anlegen
+		boolean treeFront = true; // booleanvariable angelegt mit Ausgangswert true
+		int numberleafes = 0; // intvariable angelegt mit Ausgangswert 0
 		
-		System.out.println("Hello Github, I'm Patrick");
+		// Ausgabe als Text; ohne "ln" gäbe es keinen Zeilenumbruch
 		
+		System.out.println("Ist baum voraus? " + treeFront);
+		System.out.println("Anzahl Blätter: " + numberleafes);
+		System.out.println("Anzahl Blätter: " + numberleafes + "; Baum voraus " + treeFront );
 	}	
 }
